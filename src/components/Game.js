7d4ff47489f29df02347849dbb925cb2b7bd9ccd
alexandros1890 <33435633+alexandros1890@users.noreply.github.com>
2021-07-15 01:45:05 -0300

    Add files via upload
    elección de dados

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import '../App.css';
 
 function Game() {
@@ -9,33 +9,98 @@ function Game() {
     const [dado4,setTdado4]=useState(0);
     const [dado5,setTdado5]=useState(0);
     const [dado1Boolean,setDado1Boolean]=useState(false);
+    const [dado2Boolean,setDado2Boolean]=useState(false);
+    const [dado3Boolean,setDado3Boolean]=useState(false);
+    const [dado4Boolean,setDado4Boolean]=useState(false);
+    const [dado5Boolean,setDado5Boolean]=useState(false);
     
+    
+
+
     const booleanDado1 = (event)=> {
-        // setDado1Boolean( document.getElementById("d1").value);
-        // setDado1Boolean( event.target.value);
-        alert(dado1Boolean)
+        setDado1Boolean(!dado1Boolean );
+       
         };
+
+    const booleanDado2 = (event)=> {
+        setDado2Boolean(!dado2Boolean );
+       
+        };
+    const booleanDado3 = (event)=> {
+        setDado3Boolean(!dado3Boolean );
+      
+        };
+
+    const booleanDado4 = (event)=> {
+        setDado4Boolean(!dado4Boolean );
+       
+        };
+
+    const booleanDado5 = (event)=> {
+        setDado5Boolean(!dado5Boolean );
+       
+        };
+
+    
+
+
+  
     function getNumero() {
     return Math.floor(Math.random() * (6 - 1)) + 1;
 
 
   }
 
-  const cubilete =()=>{
+  const tirar1=()=>{
+    if(!dado1Boolean){
       setTdado1(getNumero());
+    }
+  }
+  const tirar2=()=>{
+    if(!dado2Boolean){
       setTdado2(getNumero());
+    }
+  }
+  const tirar3=()=>{
+    if(!dado3Boolean){
       setTdado3(getNumero());
+    }
+  }
+  const tirar4=()=>{
+    if(!dado4Boolean){
       setTdado4(getNumero());
+    }
+  }
+  const tirar5=()=>{
+    if(!dado5Boolean){
       setTdado5(getNumero());
+    }
+  }
+
+
+
+
+  const cubilete =()=>{
+      tirar1()
+      tirar2();
+      tirar3();
+      tirar4();
+      tirar5();
   }
 
 
     return (
       <div >
           <div>
-              <p>{dado1}<input id="d1" type="checkbox" value={dado1Boolean} onChange={booleanDado1} required/>{dado2}<input type="checkbox"/>{dado3}<input type="checkbox"/>{dado4}<input type="checkbox"/>{dado5}<input type="checkbox"/></p>
+              <p>
+                {dado1}<input id="d1" type="checkbox" value={dado1Boolean} onChange={booleanDado1} />
+              {dado2}<input type="checkbox"  onChange={booleanDado2} />
+              {dado3}<input type="checkbox" value={dado3Boolean} onChange={booleanDado3} />
+              {dado4}<input type="checkbox" value={dado4Boolean} onChange={booleanDado4} />
+              {dado5}<input type="checkbox" value={dado5Boolean} onChange={booleanDado5} />
+              </p>
               <button onClick={cubilete}>tirar</button>
-              
+             
               
 
           </div>
